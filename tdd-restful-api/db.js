b926b602214ce00bdbb9640d249a2414c2a724d0
taesung1993 <cheonyulin@gmail.com>
2020-11-04 17:47:47 +0900

    to save sharing users object

---
+++ tdd-restful-api/db.js
@@ -0,0 +1,8 @@
+export let users = [
+  { id: 1, name: "유재석" },
+  { id: 2, name: "지석진" },
+  { id: 3, name: "김종국" },
+  { id: 4, name: "양세찬" },
+  { id: 5, name: "송지효" },
+  { id: 6, name: "전소민" },
+];
